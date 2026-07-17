fef586c064c62d30da7bf031d3b5bb58f709644f
MariaThomas417 <marianna.thomas0712@gmail.com>
2026-07-17 19:37:15 +0530

    Add files via upload
    Output:
    Employee ID: 201
    Name: John
    Salary: 45000

---
+++ OOP Program.py
@@ -0,0 +1,15 @@
+class Employee:
+    def __init__(self, emp_id, name, salary):
+        self.emp_id = emp_id
+        self.name = name
+        self.salary = salary
+
+    def display(self):
+        print("Employee ID:", self.emp_id)
+        print("Name:", self.name)
+        print("Salary:", self.salary)
+
+
+e1 = Employee(201, "John", 45000)
+e1.display()
+
